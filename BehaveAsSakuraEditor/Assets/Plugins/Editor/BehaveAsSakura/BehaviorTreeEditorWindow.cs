f5b2137f005f7f21802de46a7ad5a2ad9b472f1e
Wu Yuntao <wyt.brandon@gmail.com>
2017-01-17 23:20:35 +0800

    1, Fix a gitignore issue

---
+++ BehaveAsSakuraEditor/Assets/Plugins/Editor/BehaveAsSakura/BehaviorTreeEditorWindow.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace BehaveAsSakura.Editor
+{
+    public class BehaviorTreeEditorWindow : EditorWindow
+    {
+        [MenuItem("BehaveAsSakura/New Behavior Tree")]
+        private static void NewBehaviorTree()
+        {
+            //I18n.SetLanguage( "zh_CN" );
+
+            var window = GetWindow(typeof(BehaviorTreeEditorWindow));
+
+            window.titleContent = new GUIContent(I18n._("Untitled Behavior Tree"));
+        }
+
+        void OnGUI()
+        {
+            // The actual window code goes here
+        }
+    }
+}
